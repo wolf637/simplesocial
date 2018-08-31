3cb453833d07f5297df7a6d6417a84526bd7f1ed
wolf637 <alexey.volkov@live.com>
2018-08-31 16:17:16 -0700

    Add signup, login, logout flows; create views for signup/login + views for after login and after logout

--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path, re_path, include
 from simplesocial import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^$', views.HomePage.as_view(), name='home'),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^test/', views.TestPage.as_view(), name='test'),
+    url(r'^thanks/', views.ThanksPage.as_view(), name='thanks'),
 ]
