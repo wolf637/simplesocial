0dbe17659be557c20a0e98638500c0007b84ce81
wolf637 <alexey.volkov@live.com>
2018-09-01 13:06:36 -0700

    Add views for create, delete and list groups; Add templates for groups' views

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse
+from django.views import generic
+from groups.models import Group, GroupMember
 
-# Create your views here.
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+
+    fields = ('name', 'description')
+    model = Group
+
+class SingleGroup(generic.DeleteView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
